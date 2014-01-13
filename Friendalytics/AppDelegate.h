be23529d1b8e9425fa85d2d040c0c3f1aa31c263
Ethan Wessel <ejwessel@ethanwesmacbook.gateway.2wire.net>
2014-01-12 20:47:57 -0800

    Initial Commit

---
+++ Friendalytics/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Friendalytics
+//
+//  Created by Ethan Wessel on 1/12/14.
+//  Copyright (c) 2014 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Friendalytics/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Friendalytics
+//
+//  Created by Ethan Wessel on 1/12/14.
+//  Copyright (c) 2014 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
