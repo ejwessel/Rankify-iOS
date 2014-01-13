afae23e1a2c65268700dd2a8bfc992cce4b1fd24
Ethan Wessel <ejwessel@ethanwesmacbook.gateway.2wire.net>
2014-01-12 21:18:27 -0800

    setup framework setup Info.plist setup ViewController to log me in

--- Friendalytics/ViewController.m
+++ Friendalytics/ViewController.m
@@ -7,21 +7,69 @@
 //
 
 #import "ViewController.h"
+#import <FacebookSDK/FacebookSDK.h>
 
-@interface ViewController ()
+@interface ViewController () <FBLoginViewDelegate>
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad{
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSArray *permissions = @[@"user_friends",
+                             @"user_events",
+                             @"user_videos",
+                             @"user_photos",
+                             @"user_status",
+                             @"friends_events",
+                             @"friends_photos",
+                             @"friends_likes"];
+    
+    FBLoginView *loginview = [[FBLoginView alloc]
+                              initWithReadPermissions:permissions];
+    
+    loginview.frame = CGRectOffset(loginview.frame, 5, 5);
+#ifdef __IPHONE_7_0
+#ifdef __IPHONE_OS_VERSION_MAX_ALLOWED
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_7_0
+    if ([self respondsToSelector:@selector(setEdgesForExtendedLayout:)]) {
+        loginview.frame = CGRectOffset(loginview.frame, 5, 25);
+    }
+#endif
+#endif
+#endif
+    loginview.delegate = self;
+    [self.view addSubview:loginview];
+    [loginview sizeToFit];
 }
 
-- (void)didReceiveMemoryWarning
-{
+- (void)loginViewFetchedUserInfo:(FBLoginView *)loginView user:(id<FBGraphUser>)user {
+//    // here we use helper properties of FBGraphUser to dot-through to first_name and
+//    // id properties of the json response from the server; alternatively we could use
+//    // NSDictionary methods such as objectForKey to get values from the my json object
+//    self.labelFirstName.text = [NSString stringWithFormat:@"Hello %@!", user.first_name];
+//    // setting the profileID property of the FBProfilePictureView instance
+//    // causes the control to fetch and display the profile picture for the user
+//    self.profilePic.profileID = user.id;
+//    self.loggedInUser = user;
+    
+    //TESTING STUFF
+    [FBRequestConnection startWithGraphPath:@"me/?fields=permissions,friends.limit(5).fields(photos.limit(3))"
+                          completionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+                              if (!error) {
+                                  // Sucess! Include your code to handle the results here
+                                  NSLog(@"user events: %@", result);
+                              } else {
+                                  // An error occurred, we need to handle the error
+                                  // See: https://developers.facebook.com/docs/ios/errors
+                                  NSLog(@"data not retrieved");
+                              }
+                          }];
+}
+
+- (void)didReceiveMemoryWarning{
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
